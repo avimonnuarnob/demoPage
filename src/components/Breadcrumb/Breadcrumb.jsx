0e849404ec9b57d61aca2d9a5c3446ba4e2f2610
Avimonnu Arnob <avimonnuarnob@gmail.com>
2022-07-15 18:04:04 +0600

    breadcrumb

---
+++ src/components/Breadcrumb/Breadcrumb.jsx
@@ -0,0 +1,23 @@
+import useBreadcrumb from "../../hooks/useBreadcrumb";
+import styles from "./breadcrumb.module.css";
+
+const Breadcrumb = () => {
+  const { state: breadcrumbs } = useBreadcrumb();
+  return (
+    <nav>
+      <ol className={[styles["breadcrumb"]].join(" ")}>
+        {breadcrumbs?.map((el) => {
+          return (
+            <li key={el.title}>
+              <a href="#0" className={el.current ? styles.active : ""}>
+                {el.title}
+              </a>
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
+  );
+};
+
+export default Breadcrumb;
